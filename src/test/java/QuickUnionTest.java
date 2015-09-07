05eb3ad1f8ee93535104bf2f0de10f868ccefa3c
AncientMariner <xander.lysak@gmail.com>
2015-09-07 20:00:04 +0300

    add quick union

---
+++ src/test/java/QuickUnionTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class QuickUnionTest {
+    @Test
+    public void testIsConnected() throws Exception {
+        QuickUnion qu = new QuickUnion(3);
+        assertFalse(qu.isConnected(1, 2));
+    }
+
+    @Test
+    public void testUnion() throws Exception {
+        QuickUnion qu = new QuickUnion(3);
+        assertFalse(qu.isConnected(1, 2));
+        qu.union(1, 2);
+        assertTrue(qu.isConnected(1, 2));
+
+    }
+}
