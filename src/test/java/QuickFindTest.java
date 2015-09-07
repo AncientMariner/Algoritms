d2f5d56a9cc14c8cc1cb69563154a3e8d16d63a6
AncientMariner <xander.lysak@gmail.com>
2015-09-07 18:48:08 +0300

    initial commit

---
+++ src/test/java/QuickFindTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class QuickFindTest {
+    @Test
+    public void testIsConnected() {
+        QuickFind qf = new QuickFind(5);
+        assertFalse(qf.isConnected(1, 2));
+    }
+
+    @Test
+    public void testUnion() {
+        QuickFind qf = new QuickFind(5);
+        assertFalse(qf.isConnected(1, 2));
+
+        qf.union(1, 2);
+        assertTrue(qf.isConnected(1, 2));
+    }
+}
