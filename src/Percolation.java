fe14be9dfb6f0fdc8931a7f508d58e7cf918c663
Alexander Lysak <alexander.lysak@hp.com>
2015-09-21 10:59:46 +0300

    initial commit

---
+++ src/Percolation.java
@@ -0,0 +1,211 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+/**
+ * Percolation class. Backed by Weighted Quick Union model
+ * which used to store connection between open sites
+ */
+public class Percolation {
+    /**
+     * stores open sites
+     */
+    private final boolean[][] grid;
+    /**
+     * connection between open sites
+     */
+    private final WeightedQuickUnionUF model;
+    /**
+     * virtual top element
+     */
+    private final int topVirtualElement;
+    /**
+     * virtual bottom element
+     */
+    private final int bottomVirtualElement;
+    /**
+     * beginning index
+     */
+    private int begin = 1;
+    /**
+     * ending index
+     */
+    private int end;
+
+    /**
+     * creates N-by-N grid, with all sites blocked
+     * @param n - size of the grid
+     */
+    public Percolation(final int n) {
+        if (n <= 0) {
+           throw new IllegalArgumentException("Size could not be less than 0");
+        }
+        end = n + 1;
+
+        topVirtualElement = end * end;
+        bottomVirtualElement = end * end + 1;
+        grid = new boolean[end][end];
+        model = new WeightedQuickUnionUF(end * end + 2);
+    }
+
+    /**
+     * opens an element in grid
+     * @param i - row
+     * @param j - column
+     */
+    public void open(final int i, final int  j) {
+        checkBoundary(i, j);
+
+        grid[i][j] = true;
+
+        connectVirtualTop(i, j);
+        connectVirtualBottom(i, j);
+
+        checkLeftNeighbour(i, j);
+        checkRightNeighbour(i, j);
+        checkTopNeighbour(i, j);
+        checkBottomNeighbour(i, j);
+    }
+
+    /**
+     * checks whether element is opened
+     * @param i - row
+     * @param j - column
+     * @return true if opened
+     */
+    public boolean isOpen(final int i, final int j) {
+        checkBoundary(i, j);
+        return grid[i][j];
+    }
+
+    /**
+     * checks whether grid percolates
+     * @return true if percolates
+     */
+    public boolean percolates() {
+        return model.connected(topVirtualElement, bottomVirtualElement);
+    }
+
+    /**
+     * checks whether element is full
+     * @param i - row
+     * @param j - column
+     * @return true is is full
+     */
+    public boolean isFull(final int i, final int j) {
+        checkBoundary(i, j);
+        if (isOpen(i, j)) {
+            return model.connected(getAbsoluteCoordinate(i, j), topVirtualElement);
+        }
+        return false;
+    }
+
+    /**
+     * connects current element to the virtual top element
+     * @param i - row
+     * @param j - column
+     */
+    private void connectVirtualTop(final int  i, final int j) {
+        int current = getAbsoluteCoordinate(i, j);
+        if (i == begin) {
+            model.union(current, topVirtualElement);
+        }
+    }
+
+    /**
+     * connects current element to the virtual bottom element
+     * @param i - row
+     * @param j - column
+     */
+    private void connectVirtualBottom(final int  i, final int  j) {
+        int current = getAbsoluteCoordinate(i, j);
+        if (i == end - 1) {
+            model.union(current, bottomVirtualElement);
+        }
+    }
+
+    /**
+     * checks whether left neighbour is existed
+     * and connects it to the current element
+     * @param i - row
+     * @param j - column
+     */
+    private void checkLeftNeighbour(final int  i, final int  j) {
+        int current = getAbsoluteCoordinate(i, j);
+        if (j > begin && isOpen(i, j - 1)) {
+            model.union(current, checkNeighbour(i, j - 1));
+        }
+    }
+
+    /**
+     * checks whether right neighbour is existed
+     * and connects it to the current element
+     * @param i - row
+     * @param j - column
+     */
+    private void checkRightNeighbour(final int  i, final int j) {
+        int current = getAbsoluteCoordinate(i, j);
+        if (j < end - 1 && isOpen(i, j + 1)) {
+            model.union(current, checkNeighbour(i, j + 1));
+        }
+    }
+
+    /**
+     * checks whether top neighbour is existed
+     * and connects it to the current element
+     * @param i - row
+     * @param j - column
+     */
+    private void checkTopNeighbour(final int  i, final int j) {
+        int current = getAbsoluteCoordinate(i, j);
+        if (i > begin && isOpen(i - 1, j)) {
+            model.union(current, checkNeighbour(i - 1, j));
+        }
+    }
+
+    /**
+     * checks whether bottom neighbour is existed
+     * and connects it to the current element
+     * @param i - row
+     * @param j - column
+     */
+    private void checkBottomNeighbour(final int i, final int j) {
+        int current = getAbsoluteCoordinate(i, j);
+        if (i < end - 1 && isOpen(i + 1, j)) {
+            model.union(current, checkNeighbour(i + 1, j));
+        }
+    }
+
+    /**
+     * gets an absolute coordinate from i and j
+     * @param i - row
+     * @param j - column
+     * @return value of the absolute coordinate
+     */
+    private int getAbsoluteCoordinate(final int i, final int j) {
+        return i * end + j;
+    }
+
+
+    /**
+     * checks neighbour is existing element
+     * @param i - row
+     * @param j - column
+     * @return absolute coordinate of the neighbour
+     */
+    private int checkNeighbour(final int i, final int j) {
+        checkBoundary(i, j);
+        return getAbsoluteCoordinate(i, j);
+    }
+
+    /**
+     * checks boundary of the element is not violated
+     * @param i - row
+     * @param j - column
+     */
+    private void checkBoundary(final int i, final int j) {
+        if (i < begin || i >= end || j < begin || j >= end) {
+            throw new IndexOutOfBoundsException("Index is out of bounds "
+                                                 + i + " " + j);
+        }
+    }
+}
+
---
+++ src/PercolationStats.java
@@ -0,0 +1,112 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+/**
+ * Used to perform Monte-Carlo simulation upon N x N matrix
+ */
+public class PercolationStats {
+    // threshold factor
+    private static final double THRESHOLD_FACTOR = 1.96;
+    //array of the threshold values
+    private double[] threshold;
+    //number of experiments
+    private int numberOFExperiments;
+    //size of the percolation grid
+
+    /**
+     * performs independent experiments on an size-by-size grid
+     * @param size number of grid
+     * @param numberOFExperiments number of experiments
+     */
+    public PercolationStats(int size, int numberOFExperiments) {
+        threshold = new double[numberOFExperiments];
+        this.numberOFExperiments = numberOFExperiments;
+
+        for (int i = 0; i < numberOFExperiments; i++) {
+            int openCounter = calculate(size);
+            threshold[i] = (double) openCounter / (size * size);
+        }
+    }
+
+    /**+
+     * calculates the percolation of the grid
+     * @param n size of the grid
+     * @return number of opened positions
+     */
+    private int calculate(final int n) {
+        Percolation percolation = new Percolation(n);
+        int openCounter = 0;
+
+        while (!percolation.percolates()) {
+            int x = StdRandom.uniform(1, n);
+            int y = StdRandom.uniform(1, n);
+
+            if (!percolation.isOpen(x, y)) {
+               percolation.open(x, y);
+               openCounter++;
+            }
+        }
+        return openCounter;
+    }
+
+    /**
+     * calculates the percolation threshold
+     * @return value of the threshold
+     */
+    public double mean() {
+        return StdStats.mean(threshold);
+    }
+
+    /**
+     * calculates the sample standard deviation of percolation threshold,
+     * sharpness of the threshold
+     * @return value of the sharpness of the threshold
+     */
+    public double stddev() {
+        if (numberOFExperiments == 1) {
+            return Double.NaN;
+        }
+        return StdStats.stddev(threshold);
+    }
+
+    /**
+     * low endpoint of 95% confidence interval
+     * @return value of the low endpoint interval
+     */
+    public double confidenceLo() {
+        return mean() - (THRESHOLD_FACTOR * stddev()
+                / Math.sqrt(numberOFExperiments));
+    }
+
+    /**
+     * high endpoint of 95% confidence interval
+     * @return value of the high endpoint interval
+     */
+    public double confidenceHi() {
+        return mean() + (THRESHOLD_FACTOR * stddev()
+                / Math.sqrt(numberOFExperiments));
+    }
+
+    /**
+     *  test client
+     * @param args values to enter
+     */
+    public static void main(String[] args) {
+        StdOut.println("Please state the number of experiments ");
+        int numberOfExperiments = StdIn.readInt();
+        StdOut.println("Please state the size of area ");
+        int size = StdIn.readInt();
+
+        PercolationStats percolationStats = new PercolationStats(size, numberOfExperiments);
+        double mean = percolationStats.mean();
+        double stddev = percolationStats.stddev();
+        double min = percolationStats.confidenceLo();
+        double max = percolationStats.confidenceHi();
+
+        StdOut.println("mean " + mean);
+        StdOut.println("stddev " + stddev);
+        StdOut.println("95% confidence interval = " + min + " - " + max);
+    }
+}
