d2490e223570273f8d0f30abf744abe6f6eb0221
AncientMariner <xander.lysak@gmail.com>
2015-10-05 19:18:57 +0300

    week 3. Collinear points. Initial commit

---
+++ src/test/java/collinear/PointTest.java
@@ -0,0 +1,41 @@
+package collinear;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PointTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testDraw() throws Exception {
+
+    }
+
+    @Test
+    public void testDrawTo() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void testCompareTo() throws Exception {
+
+    }
+
+    @Test
+    public void testSlopeTo() throws Exception {
+
+    }
+
+    @Test
+    public void testSlopeOrder() throws Exception {
+
+    }
+}
