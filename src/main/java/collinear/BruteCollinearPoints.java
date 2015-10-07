bf1dbfa07b40060844414c434d8baf7537e284f7
AncientMariner <xander.lysak@gmail.com>
2015-10-07 17:59:15 +0300

    brute force collinear points initialised

---
+++ src/main/java/collinear/BruteCollinearPoints.java
@@ -0,0 +1,15 @@
+package collinear;
+
+public class BruteCollinearPoints {
+   public BruteCollinearPoints(Point[] points) {
+
+   } // finds all line segments containing 4 points
+
+   public int numberOfSegments() {
+       return -1;
+   } // the number of line segments
+
+   public LineSegment[] segments() {
+       return null;
+   } // the line segments
+}
---
+++ src/test/java/collinear/BruteCollinearPointsTest.java
@@ -0,0 +1,30 @@
+package collinear;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class BruteCollinearPointsTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testBasic() {
+        assertNotNull(new BruteCollinearPoints(null));
+    }
+
+
+    @Test
+    public void testNumberOfSegments() throws Exception {
+//        BruteCollinearPoints bruteCollinearPoints = new BruteCollinearPoints(null);
+
+    }
+
+    @Test
+    public void testSegments() throws Exception {
+
+    }
+}
