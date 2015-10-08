f3bbaa3e51539e7584fdef9cf0bf58942b64652b
AncientMariner <xander.lysak@gmail.com>
2015-10-08 18:18:42 +0300

    brute collinear working no duplicates

--- src/main/java/collinear/BruteCollinearPoints.java
+++ src/main/java/collinear/BruteCollinearPoints.java
@@ -13,26 +13,24 @@ public class BruteCollinearPoints {
         if (checkForCornerCases(points)) return;
 
         double slope01;
-        double slope12;
-        double slope23;
-        List<Point> pointList = new ArrayList<>();
-        for (int i = 0; i < points.length - 1; i++) {
-            if (points[i].compareTo(points[i + 1]) != 0) {
-                pointList.add(points[i]);
-            }
-        }
-        if (points[points.length - 1].compareTo(points[0]) != 0) {
-            pointList.add(points[points.length - 1]);
-        }
+        double slope02;
+        double slope03;
+
+        List<Point> pointList = identifyUniquePoints(points);
+
         this.points = new Point[pointList.size()];
         for (int i = 0; i < pointList.size(); i++) {
             this.points[i] = pointList.get(i);
         }
-
         Arrays.sort(this.points);
+
         lineSegments = new LineSegment[this.points.length];
 
         Point max = this.points[0];
+        Point previousI = this.points[0];
+        Point previousJ = this.points[0];
+        Point previousK = this.points[0];
+        Point previousL = this.points[0];
         for (int i = 0; i < this.points.length; i++) {
             for (int j = 0 + i; j < this.points.length; j++) {
                 for (int k = 0 + j; k < this.points.length; k++) {
@@ -40,16 +38,26 @@ public class BruteCollinearPoints {
                         if (i != j && i != k && i != l
                                 && j != k && j != l
                                 && k != l) {
-                            slope01 = this.points[i].slopeTo(this.points[j]);
-                            slope12 = this.points[j].slopeTo(this.points[k]);
-                            slope23 = this.points[k].slopeTo(this.points[l]);
-                            if (slope01 == slope12 && slope12 == slope23
-                             && slope01 == slope23) {
-                                if (numberOfSegments < this.points.length) {
-                                    if (max.compareTo(this.points[l]) < 0) {
-                                        max = this.points[l];
+                            if (previousJ.compareTo(points[j]) != 0
+                                    && previousK.compareTo(points[k]) != 0
+                                    && previousL.compareTo(points[l]) != 0
+                                    ) {
+                                slope01 = this.points[i].slopeTo(this.points[j]);
+                                slope02 = this.points[i].slopeTo(this.points[k]);
+                                slope03 = this.points[i].slopeTo(this.points[l]);
+                                if (slope01 == slope02
+                                        && slope02 == slope03
+                                        && slope01 == slope03) {
+                                    if (numberOfSegments < this.points.length) {
+                                        if (max.compareTo(this.points[l]) < 0) {
+                                            max = this.points[l];
+                                        }
                                         lineSegments[numberOfSegments++]
-                                        = new LineSegment(this.points[i], max);
+                                                = new LineSegment(this.points[i], this.points[l]);
+//                                    previousI = points[i];
+                                        previousJ = points[j];
+                                        previousK = points[k];
+                                        previousL = points[l];
                                     }
                                 }
                             }
@@ -59,6 +67,19 @@ public class BruteCollinearPoints {
             }
         }
     } // finds all line segments containing 4 points
+
+    private List<Point> identifyUniquePoints(Point[] points) {
+        List<Point> pointList = new ArrayList<>();
+        for (int i = 0; i < points.length - 1; i++) {
+            if (points[i].compareTo(points[i + 1]) != 0) {
+                pointList.add(points[i]);
+            }
+        }
+        if (points[points.length - 1].compareTo(points[0]) != 0) {
+            pointList.add(points[points.length - 1]);
+        }
+        return pointList;
+    }
 
     private boolean checkForCornerCases(Point[] points) {
         if (points == null || points.length == 0) {
