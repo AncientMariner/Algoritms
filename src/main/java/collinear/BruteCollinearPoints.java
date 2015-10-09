1854a8b2f8edb7a27725a41e232b5ac3ee234079
AncientMariner <xander.lysak@gmail.com>
2015-10-09 10:49:25 +0300

    added tree set for checking on duplicate points

--- src/main/java/collinear/BruteCollinearPoints.java
+++ src/main/java/collinear/BruteCollinearPoints.java
@@ -8,10 +8,30 @@ public class BruteCollinearPoints {
     private int numberOfSegments;
 
     public BruteCollinearPoints(Point[] points) {
-        if (checkForCornerCases(points)) return;
-
-        List<Point> pointList = identifyUniquePoints(points);
-
+        if (points == null || points.length == 0) {
+            throw new NullPointerException("there are no points");
+        }
+        if (points.length > 0) {
+            for (int i = 0; i < points.length; i++) {
+                if (points[i] == null) {
+                    throw new NullPointerException("one of the points is null");
+                }
+            }
+        }
+        Set<Point> pointSet = new TreeSet<>();
+        if (points.length > 1) {
+            for (int i = 0; i < points.length; i++) {
+                pointSet.add(points[i]);
+            }
+            if (pointSet.size() != points.length) {
+                throw new IllegalArgumentException("there is a duplicate");
+            }
+        }
+        if (points.length == 1) {
+            return;
+        }
+        pointSet.toArray();
+        List<Point> pointList = new ArrayList<>(pointSet);
         this.points = new Point[pointList.size()];
         for (int i = 0; i < pointList.size(); i++) {
             this.points[i] = pointList.get(i);
@@ -38,10 +58,13 @@ public class BruteCollinearPoints {
                                 slope02 = this.points[i].slopeTo(this.points[k]);
                                 if(slope01 == slope02)
                                     slope03 = this.points[i].slopeTo(this.points[l]);
-                                if (slope01 == slope02 && slope02 == slope03 && slope01 == slope03) {
+                                if (slope01 == slope02
+                                        && slope02 == slope03
+                                        && slope01 == slope03) {
                                     if (numberOfSegments < this.points.length) {
                                         lineSegments[numberOfSegments++]
-                                                = new LineSegment(this.points[i], this.points[l]);
+                                                = new LineSegment(this.points[i],
+                                                this.points[l]);
                                         prevJ = this.points[j];
                                         prevK = this.points[k];
                                         prevL = this.points[l];
@@ -54,45 +77,6 @@ public class BruteCollinearPoints {
             }
         }
     } // finds all line segments containing 4 points
-
-    private List<Point> identifyUniquePoints(Point[] points) {
-        List<Point> pointList = new ArrayList<>();
-        for (int i = 0; i < points.length - 1; i++) {
-            if (points[i].compareTo(points[i + 1]) != 0) {
-                pointList.add(points[i]);
-            }
-        }
-        if (points[points.length - 1].compareTo(points[0]) != 0) {
-            pointList.add(points[points.length - 1]);
-        }
-        return pointList;
-    }
-
-    private boolean checkForCornerCases(Point[] points) {
-        if (points == null || points.length == 0) {
-            throw new NullPointerException("there are no points");
-        }
-        if (points.length > 0) {
-            for (int i = 0; i < points.length; i++) {
-                if (points[i] == null) {
-                    throw new NullPointerException("one of the points is null");
-                }
-            }
-        }
-        if (points.length > 1) {
-            Set<Point> pointSet = new TreeSet<>();
-            for (int i = 0; i < points.length; i++) {
-                pointSet.add(points[i]);
-            }
-            if (pointSet.size() != points.length) {
-                throw new IllegalArgumentException("there is a duplicate");
-            }
-        }
-        if (points.length == 1) {
-            return true;
-        }
-        return false;
-    }
 
     public LineSegment[] segments() {
         LineSegment[] copyOfLineSegments = new LineSegment[numberOfSegments];
